d4a44dcdb92075699e14df5b164ce5ab56cf95e6
peter-conder <pconder2005@gmail.com>
2026-08-12 16:05:39 -0600

    make gameboard array of objects with magic numbers for row validation

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const gameBoard = 
-    [1,2,3,4,5,6,7,8,9];
-
-//console.log(gameBoard);
-//console.log(gameBoard.at[1],[1]);
+    const ticTacToe = (() => {
+    //this is now one massive object! no global code whatsover.
+    //Just have to be careful where I put parentheses and stuff.
+    
+    const gameBoard = [
+        {"magicNumber": 2, "playerInfo": null},
+        {"magicNumber": 7, "playerInfo": null},
+        {"magicNumber": 6, "playerInfo": null},
+        {"magicNumber": 9, "playerInfo": null},
+        {"magicNumber": 5, "playerInfo": null},
+        {"magicNumber": 1, "playerInfo": null},
+        {"magicNumber": 4, "playerInfo": null},
+        {"magicNumber": 3, "playerInfo": null},
+        {"magicNumber": 8, "playerInfo": null},
+    ]
 
 function createPlayer(name, icon) {
     const playerName = name;
@@ -13,16 +23,18 @@ function createPlayer(name, icon) {
 
 const human = createPlayer('human', 'x');
 const bot = createPlayer('bot', 'o');
-//console.log(human);
-//console.log(bot);
 
 const gameFlow = (() => {
     const placeOnBoard = (square) => {
-        gameBoard.splice(square, 1, human);
+        gameBoard[square].playerInfo = 'human';
         return console.log(gameBoard);
     };
+
+
 
 return {placeOnBoard};
 })();
 
-console.log(gameFlow.placeOnBoard(1));
+console.log(gameFlow.placeOnBoard(prompt("what do you want to play?")));
+
+})();
