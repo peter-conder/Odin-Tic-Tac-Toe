a5aeea392f13ddf805ef029d47b83cf38070c633
peter-conder <pconder2005@gmail.com>
2026-08-12 15:09:47 -0600

    add placeOnBoard function that places object onto gameBoard array

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-/**
- * Need 4 objects:
-     * gameBoard: holds a 3x3 matrix that x's or o's are stored in.
-     * gameFlow: has each player take a turn placing an x or an o
-     * player1: holds player1's name and his icons
-     * player2: holds player2's name and his icons
- * 
- */
+const gameBoard = 
+    [1,2,3,4,5,6,7,8,9];
 
-const gameBoard = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-
-];
+//console.log(gameBoard);
+//console.log(gameBoard.at[1],[1]);
 
 function createPlayer(name, icon) {
     const playerName = name;
     const playerIcon = icon;
     //maybe add a different parameter for wins later
-
     return {playerName, playerIcon};
 }
 
 const human = createPlayer('human', 'x');
 const bot = createPlayer('bot', 'o');
+//console.log(human);
+//console.log(bot);
 
-console.log(human);
-console.log(bot);
+const gameFlow = (() => {
+    const placeOnBoard = (square) => {
+        gameBoard.splice(square, 1, human);
+        return console.log(gameBoard);
+    };
+
+return {placeOnBoard};
+})();
+
+console.log(gameFlow.placeOnBoard(1));
