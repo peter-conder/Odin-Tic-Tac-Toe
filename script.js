6f0eae5808c9460b543d01904638e2d4b14a86ba
peter-conder <pconder2005@gmail.com>
2026-08-13 11:21:04 -0600

    add setup function with gameBoard and player initialization

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     
-    (function () {const gameBoard = [
-        {"magicNumber": 2, "playerInfo": null},
+    const setup = (() => {
+        const gameBoard = [{"magicNumber": 2, "playerInfo": null},
         {"magicNumber": 7, "playerInfo": null},
         {"magicNumber": 6, "playerInfo": null},
         {"magicNumber": 9, "playerInfo": null},
@@ -8,35 +8,48 @@
         {"magicNumber": 1, "playerInfo": null},
         {"magicNumber": 4, "playerInfo": null},
         {"magicNumber": 3, "playerInfo": null},
-        {"magicNumber": 8, "playerInfo": null},]
-    })();
-(function ()
-    {function createPlayer(name, icon) {
-    const playerName = name;
-    const playerIcon = icon;
-    //maybe add a different parameter for wins later
-    return {playerName, playerIcon};
-        }
-    const human = createPlayer('human', 'x');
-    const bot = createPlayer('bot', 'o');}
-    )();
+        {"magicNumber": 8, "playerInfo": null},];
+        
+
+        function createPlayer(name, icon) {
+        let playerName = name;
+        let playerIcon = icon;
+        //maybe add a different parameter for wins later
+        return {playerName, playerIcon}; //<returns the player object
+        };
+
+        const player1 = createPlayer('player1', 'x');
+        const player2 = createPlayer('player2', 'o');
+
+        return {gameBoard, player1, player2};
+        })();
+        console.log(setup.gameBoard);
+        console.log(setup.player1);
+        console.log(setup.player2);  
+
+        //gameSetup: has gameboard (returns with gameSetup.gameBoard) and players (gameSetup.player1/player2)
+
 
 const gamePlay = (() => {
     const placeOnBoard = (square) => {
-        gameBoard[square].playerInfo = human;
+        gameBoard[square].playerInfo = player1;
         console.log(gameBoard);
+    const checkWin = () => {
+        //need some way to see if player is in square: use gameBoard.playerInfo = {object}, then access what that object is
+        //can access checkWin with gamePlay.checkWin
+        //maybe can use whoPlayed (who just played a piece) to check, bc you only need to see if the piece you just played will result in a win or not
+        //if ROW add up to 15 {}
+        //if COLUMN add up to 15 {}
+        //if DIAGONAL add up to 15 {}
+    }
     };
 
 return {placeOnBoard};
 })();
 
-//making a new function referencing events in gameFlow DOES work.
-
 const gameFlow = (() => {
-    function isWin() {
-    };
+        //gamePlay.placeOnBoard(prompt("Where would you like to play?"));
+    //(function isWin() {
+    //})();
 })();
-
-//gamePlace.placeOnBoard(0);
-//gamePlace.botPlaysPiece();
 
