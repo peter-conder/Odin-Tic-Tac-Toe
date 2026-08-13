dabd63f2e2516ef469f9f8792e284ad689c52a9d
peter-conder <pconder2005@gmail.com>
2026-08-13 09:57:19 -0600

    fix function scope, no global code exists

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-    const ticTacToe = (() => {
-    //this is now one massive object! no global code whatsover.
-    //Just have to be careful where I put parentheses and stuff.
     
-    const gameBoard = [
+    (function () {const gameBoard = [
         {"magicNumber": 2, "playerInfo": null},
         {"magicNumber": 7, "playerInfo": null},
         {"magicNumber": 6, "playerInfo": null},
@@ -11,30 +8,35 @@
         {"magicNumber": 1, "playerInfo": null},
         {"magicNumber": 4, "playerInfo": null},
         {"magicNumber": 3, "playerInfo": null},
-        {"magicNumber": 8, "playerInfo": null},
-    ]
-
-function createPlayer(name, icon) {
+        {"magicNumber": 8, "playerInfo": null},]
+    })();
+(function ()
+    {function createPlayer(name, icon) {
     const playerName = name;
     const playerIcon = icon;
     //maybe add a different parameter for wins later
     return {playerName, playerIcon};
-}
+        }
+    const human = createPlayer('human', 'x');
+    const bot = createPlayer('bot', 'o');}
+    )();
 
-const human = createPlayer('human', 'x');
-const bot = createPlayer('bot', 'o');
-
-const gameFlow = (() => {
+const gamePlay = (() => {
     const placeOnBoard = (square) => {
-        gameBoard[square].playerInfo = 'human';
-        return console.log(gameBoard);
+        gameBoard[square].playerInfo = human;
+        console.log(gameBoard);
     };
-
-
 
 return {placeOnBoard};
 })();
 
-console.log(gameFlow.placeOnBoard(prompt("what do you want to play?")));
+//making a new function referencing events in gameFlow DOES work.
 
+const gameFlow = (() => {
+    function isWin() {
+    };
 })();
+
+//gamePlace.placeOnBoard(0);
+//gamePlace.botPlaysPiece();
+
