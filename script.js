1bcedd07e103db9c25342a37febf4661d4233c2b
peter-conder <pconder2005@gmail.com>
2026-08-13 12:14:37 -0600

    remove magicNumbers

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
     
     const setup = (() => {
-        const gameBoard = [{"magicNumber": 2, "playerInfo": null},
-        {"magicNumber": 7, "playerInfo": null},
-        {"magicNumber": 6, "playerInfo": null},
-        {"magicNumber": 9, "playerInfo": null},
-        {"magicNumber": 5, "playerInfo": null},
-        {"magicNumber": 1, "playerInfo": null},
-        {"magicNumber": 4, "playerInfo": null},
-        {"magicNumber": 3, "playerInfo": null},
-        {"magicNumber": 8, "playerInfo": null},];
+        const gameBoard = [
+        {"playerInfo": null},
+        {"playerInfo": null},
+        {"playerInfo": null},
+        {"playerInfo": null},
+        {"playerInfo": null},
+        {"playerInfo": null},
+        {"playerInfo": null},
+        {"playerInfo": null},
+        {"playerInfo": null},];
         
-
         function createPlayer(name, icon) {
         let playerName = name;
         let playerIcon = icon;
@@ -23,20 +23,18 @@
 
         return {gameBoard, player1, player2};
         })();
-        console.log(setup.gameBoard);
-        console.log(setup.player1);
-        console.log(setup.player2);  
+        //console.log(setup.gameBoard);
+        //console.log(setup.player1);
+        //console.log(setup.player2);  
 
         //gameSetup: has gameboard (returns with gameSetup.gameBoard) and players (gameSetup.player1/player2)
 
 
 const gamePlay = (() => {
-    const placeOnBoard = (square) => {
-        gameBoard[square].playerInfo = player1;
-        console.log(gameBoard);
+    const placeOnBoard = (square, player) => {
+        setup.gameBoard[square].playerInfo = player;
     const checkWin = () => {
         //need some way to see if player is in square: use gameBoard.playerInfo = {object}, then access what that object is
-        //can access checkWin with gamePlay.checkWin
         //maybe can use whoPlayed (who just played a piece) to check, bc you only need to see if the piece you just played will result in a win or not
         //if ROW add up to 15 {}
         //if COLUMN add up to 15 {}
@@ -47,9 +45,12 @@ const gamePlay = (() => {
 return {placeOnBoard};
 })();
 
+gamePlay.placeOnBoard(1, setup.player1);
+gamePlay.placeOnBoard(3, setup.player2);
+console.log(setup.gameBoard);
+console.log(setup.gameBoard[3].playerInfo.playerIcon); //<-- maybe add function so that we can access playerIcons easily without needing this ridiculous string of object properties
+
 const gameFlow = (() => {
-        //gamePlay.placeOnBoard(prompt("Where would you like to play?"));
-    //(function isWin() {
-    //})();
+
 })();
 
