c86d096af32f67d0f4f3a38f52eac68de04d566e
peter-conder <pconder2005@gmail.com>
2026-08-30 12:31:39 -0500

    add boardID to each square

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
     
     const setup = (() => {
         const gameBoard = [
-        {"playerInfo": null},
-        {"playerInfo": null},
-        {"playerInfo": null},
-        {"playerInfo": null},
-        {"playerInfo": null},
-        {"playerInfo": null},
-        {"playerInfo": null},
-        {"playerInfo": null},
-        {"playerInfo": null},];
+        {"playerInfo": null, "boardID": 0},
+        {"playerInfo": null, "boardID": 1},
+        {"playerInfo": null, "boardID": 2},
+        {"playerInfo": null, "boardID": 3},
+        {"playerInfo": null, "boardID": 4},
+        {"playerInfo": null, "boardID": 5},
+        {"playerInfo": null, "boardID": 6},
+        {"playerInfo": null, "boardID": 7},
+        {"playerInfo": null, "boardID": 8},];
         
         function createPlayer(name, icon) {
         let playerName = name;
@@ -20,62 +20,60 @@
 
         const player1 = createPlayer('player1', 'x');
         const player2 = createPlayer('player2', 'o');
+        //creates players
 
-        function makeWinArray(arr) {
-            let arr1 = arr.slice(0,3);
-            let arr2 = arr.slice(3,6);
-            let arr3 = arr.slice(6);
-            let arr4 = ((arr) => {
+        function makeWinArray(parentArray) {
+            let arr1 = parentArray.slice(0,3);
+            let arr2 = parentArray.slice(3,6);
+            let arr3 = parentArray.slice(6);
+            let arr4 = (() => {
                 let array = [];
                 array.push(gameBoard[0]);
                 array.push(gameBoard[3]);
                 array.push(gameBoard[6]);
                 return array;
-            })();
-
-            let arr5 = ((arr) => {
+                    })();
+                
+            let arr5 = (() => {
                 let array = [];
                 array.push(gameBoard[1]);
                 array.push(gameBoard[4]);
                 array.push(gameBoard[7]);
                 return array;
-
-            })();let arr6 = ((arr) => {
+                    })();
+            
+            let arr6 = (() => {
                 let array = [];
                 array.push(gameBoard[2]);
                 array.push(gameBoard[5]);
                 array.push(gameBoard[8]);
                 return array;
+                    })();
 
-            })();let arr7 = ((arr) => {
+            let arr7 = (() => {
                 let array = [];
                 array.push(gameBoard[0]);
                 array.push(gameBoard[4]);
                 array.push(gameBoard[8]);
                 return array;
-
-            })();let arr8 = ((arr) => {
+                    })();
+    
+            let arr8 = (() => {
                 let array = [];
                 array.push(gameBoard[2]);
                 array.push(gameBoard[4]);
                 array.push(gameBoard[6]);
                 return array;
-
-            })();
+                    })();
             let winArray1 = [arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8];
             return winArray1;
         }
 
-        const winArray1 = makeWinArray(gameBoard);
+        const winArray = makeWinArray(gameBoard);
 
-        return {gameBoard, player1, player2, winArray1};
+        return {gameBoard, player1, player2, winArray};
         })();
-        //console.log(setup.gameBoard);
-        //console.log(setup.player1);
-        //console.log(setup.player2);  
 
-        //gameSetup: has gameboard (returns with gameSetup.gameBoard) and players (gameSetup.player1/player2)
-        console.log(setup.winArray1);
 
 const gamePlay = (() => {
         let canPlace = true;
@@ -96,35 +94,51 @@ const gamePlay = (() => {
         placeOnBoard(differentSquare, player);
     };
 }
-return {placeOnBoard};
+
+const isWin = () => {
+    let s = 0;
+    for (let t = 0; t < 8; t++) {
+    let subArray = setup.winArray[s];
+    let isEqual = arr => arr.every(item => item === arr[0]);
+    if (isEqual(subArray) === true) {
+        console.log('somebody won');
+    }  else {
+        console.log("nobody won");
+    }
+    s++;
+   };
+};
+return {placeOnBoard, isWin};
 })();
 
-//gamePlay.placeOnBoard(1, setup.player1);
-//gamePlay.placeOnBoard(3, setup.player2);
-//console.log(setup.gameBoard);
 //console.log(setup.gameBoard[3].playerInfo.playerIcon); //<-- maybe add function so that we can access playerIcons easily without needing this ridiculous string of object properties
 
 const gameFlow = (() => {
-    //gamePlay.placeOnBoard(prompt("where would you like to place?"), setup.player1);
-    //gamePlay.placeOnBoard(prompt("where would you like to place?"), setup.player2);
-    //console.log(setup.gameBoard[1].playerInfo.playerIcon);
+    gamePlay.placeOnBoard(prompt("player1"), setup.player1);
+    gamePlay.placeOnBoard(prompt("player1"), setup.player1);
+    gamePlay.placeOnBoard(prompt("player1"), setup.player1);
+    gamePlay.isWin();
+    console.log(setup.gameBoard);
 })();
-function setPlayerInfo(arr) {
-    let j = 0;
-    for (let i = 0; i < 9; i++) {
-        arr[j].playerInfo = j;
-        j++;
-    }
-}
-setPlayerInfo(setup.gameBoard);
-console.log(setup.gameBoard);
+
+
+
+//sets playerInfo to 0-8 on each array square
+//I made this little loop to practice loops I guess. I could have done this manually but no
+
+//-------------------------------------
+//setPlayerInfo(setup.gameBoard);
+//console.log(setup.gameBoard);
+//console.log(setup.winArray[0][1]);
+//-------------------------------------
 
 //can now place icons in array.
 //what now? need a way to check win condition.
 //easiest way to implement this is to make a gamePlay.isWin object that can play after turn 5 in the gameFlow object.
+
 /** isWin
  * check the contents of specific sub-arrays
- * sub-arrays: 1,2,3 || 4,5,6 || 7,8,9 || 1,4,7 || 2,5,8 || 3,6,9 || 1,5,9 || 3,5,7 
+ * sub-arrays: 1,2,3 || 4,5,6 || 7,8,9 || 1,4,7 || 2,5,8 || 3,6,9 || 1,5,9 || 3,5,7
+ 
  * loop through each of these sub-arrays, if any of these sub-arrays have the same player object in all of them, then that player wins
- * maybe make this an array of arrays? winArray = array of these arrays, made using splice
  */
