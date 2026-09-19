4c8fcee7af9b8aee6608ed911f819c390677f99b
peter-conder <pconder2005@gmail.com>
2026-09-19 14:43:27 -0500

    add basic html layout and css styling

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-    
     const setup = (() => {
         const gameBoard = [
         {"playerInfo": null, "boardID": 0},
@@ -12,20 +11,45 @@
         {"playerInfo": null, "boardID": 8},];
         
         function createPlayer(name, icon) {
-        let playerName = name;
-        let playerIcon = icon;
-        //maybe add a different parameter for wins later
-        return {playerName, playerIcon}; //<returns the player object
+            let playerName = name;
+            let playerIcon = icon;
+                //maybe add a different parameter for wins later
+            return {playerName, playerIcon}; //<returns the player object
         };
 
         const player1 = createPlayer('player1', 'x');
         const player2 = createPlayer('player2', 'o');
-        //creates players
 
-        function makeWinArray(parentArray) {
-            let arr1 = parentArray.slice(0,3);
-            let arr2 = parentArray.slice(3,6);
-            let arr3 = parentArray.slice(6);
+        // 0 | 1 | 2
+        // 3 | 4 | 5
+        // 6 | 7 | 8
+
+        function makeWinArray() {
+            //makes the mini-arrays that will check if a player has won or not
+            let arr1 = (() => {
+                let array = [];
+                array.push(gameBoard[0]);
+                array.push(gameBoard[1]);
+                array.push(gameBoard[2]);
+                return array;
+                    })();
+
+            let arr2 = (() => {
+                let array = [];
+                array.push(gameBoard[3]);
+                array.push(gameBoard[4]);
+                array.push(gameBoard[5]);
+                return array;
+                    })();
+
+            let arr3 = (() => {
+                let array = [];
+                array.push(gameBoard[6]);
+                array.push(gameBoard[7]);
+                array.push(gameBoard[8]);
+                return array;
+                     })();
+
             let arr4 = (() => {
                 let array = [];
                 array.push(gameBoard[0]);
@@ -74,9 +98,9 @@
         return {gameBoard, player1, player2, winArray};
         })();
 
-
 const gamePlay = (() => {
         let canPlace = true;
+        let continuePlay = true;
         function isOccupied(input) {
             if (setup.gameBoard[input].playerInfo === null)
                 {return;
@@ -84,7 +108,8 @@ const gamePlay = (() => {
                     console.error("that square is taken!")
                     return canPlace = false;
                 }}
-    const placeOnBoard = (square, player) => {
+
+const placeOnBoard = (square, player) => {
         isOccupied(square);
         if (canPlace === true) {
         setup.gameBoard[square].playerInfo = player;
@@ -95,13 +120,14 @@ const gamePlay = (() => {
     };
 }
 
-const isWin = () => {
+const isWin = (player) => {
     let s = 0;
     for (let t = 0; t < 8; t++) {
     let subArray = setup.winArray[s];
-    let isEqual = arr => arr.every(item => item === arr[0]);
+    let isEqual = arr => arr.every(item => item.playerInfo === arr[0].playerInfo);
     if (isEqual(subArray) === true) {
-        console.log('somebody won');
+        console.log(`somebody won, triggered at${JSON.stringify(subArray)}`);
+        return console.log(`${JSON.stringify(player.playerName)} won!`);
     }  else {
         console.log("nobody won");
     }
@@ -113,24 +139,16 @@ return {placeOnBoard, isWin};
 
 //console.log(setup.gameBoard[3].playerInfo.playerIcon); //<-- maybe add function so that we can access playerIcons easily without needing this ridiculous string of object properties
 
+
 const gameFlow = (() => {
-    gamePlay.placeOnBoard(prompt("player1"), setup.player1);
-    gamePlay.placeOnBoard(prompt("player1"), setup.player1);
-    gamePlay.placeOnBoard(prompt("player1"), setup.player1);
-    gamePlay.isWin();
-    console.log(setup.gameBoard);
+    
+    gamePlay.placeOnBoard(0, setup.player1);
+    gamePlay.placeOnBoard(1, setup.player1);
+    gamePlay.placeOnBoard(2, setup.player1);
+    gamePlay.isWin(setup.player1)
+    console.log(setup.gameBoard); 
+    
 })();
-
-
-
-//sets playerInfo to 0-8 on each array square
-//I made this little loop to practice loops I guess. I could have done this manually but no
-
-//-------------------------------------
-//setPlayerInfo(setup.gameBoard);
-//console.log(setup.gameBoard);
-//console.log(setup.winArray[0][1]);
-//-------------------------------------
 
 //can now place icons in array.
 //what now? need a way to check win condition.
@@ -138,7 +156,7 @@ const gameFlow = (() => {
 
 /** isWin
  * check the contents of specific sub-arrays
- * sub-arrays: 1,2,3 || 4,5,6 || 7,8,9 || 1,4,7 || 2,5,8 || 3,6,9 || 1,5,9 || 3,5,7
+ * sub-arrays: 1,2,3 || 4,5,6 || 6,7,8 || 1,4,7 || 2,5,8 || 3,6,9 || 1,5,9 || 3,5,7
  
  * loop through each of these sub-arrays, if any of these sub-arrays have the same player object in all of them, then that player wins
  */
